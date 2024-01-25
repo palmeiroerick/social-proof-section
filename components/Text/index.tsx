dbd2b3acee6961b38e6e739743d632aca4d29afa
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-25 18:04:45 -0300

    style: add desktop styles to text component

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -4,7 +4,7 @@ type TextProps = {
 
 const Text = ({ children }: TextProps) => {
   return (
-    <p className="text-center text-[17.5px] leading-5 text-darkGrayishMagenta">
+    <p className="text-center text-[17.5px] leading-5 font-medium text-darkGrayishMagenta lg:text-start">
       {children}
     </p>
   );
