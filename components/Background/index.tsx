83bbb73eaec9d4b647a1553e9aecf4d6477e44f2
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-25 17:15:24 -0300

    feat: add desktop background

--- components/Background/index.tsx
+++ components/Background/index.tsx
@@ -3,19 +3,35 @@ import Image from "next/image";
 const Background = () => {
   return (
     <>
+      {/* Mobile Background */}
       <Image
-        className="absolute left-1/2 top-0 -translate-x-1/2"
+        className="absolute left-1/2 top-0 -translate-x-1/2 lg:hidden"
         src="/assets/bg-pattern-top-mobile.svg"
         alt=""
         width={374}
         height={232}
       />
       <Image
-        className="absolute bottom-0 left-1/2 -translate-x-1/2"
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 lg:hidden"
         src="/assets/bg-pattern-bottom-mobile.svg"
         alt=""
         width={375}
         height={503}
+      />
+      {/* Desktop Background */}
+      <Image
+        className="absolute left-0 top-0 hidden lg:block"
+        src="/assets/bg-pattern-top-desktop.svg"
+        alt=""
+        width={584}
+        height={362}
+      />
+      <Image
+        className="absolute bottom-0 right-0 hidden lg:block"
+        src="/assets/bg-pattern-bottom-desktop.svg"
+        alt=""
+        width={1085}
+        height={673}
       />
     </>
   );
