90a621db283207590bd359795c435d05955e2be5
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-25 18:04:34 -0300

    style: add desktop styles to review component

--- components/Review/index.tsx
+++ components/Review/index.tsx
@@ -1,28 +1,36 @@
 import Image from "next/image";
 
 type ReviewProps = {
+  className?: string;
   name: string;
   authorImage: `/assets/image-${string}.jpg`;
   review: string;
 };
 
-const Review = ({ name, authorImage, review }: ReviewProps) => {
+const Review = ({ className, name, authorImage, review }: ReviewProps) => {
   return (
-    <div className="flex flex-col gap-4 rounded-lg bg-veryDarkMagenta p-7">
-      <div className="flex gap-3">
-        <Image
-          className="rounded-full"
-          src={authorImage}
-          alt=""
-          width={32}
-          height={32}
-        />
+    <div
+      className={`${className} flex flex-col gap-4 rounded-lg bg-veryDarkMagenta p-7 lg:p-8`}
+    >
+      <div className="flex gap-3 lg:gap-6">
+        <div className="relative h-8 w-8 lg:h-10 lg:w-10">
+          <Image
+            className="rounded-full object-contain"
+            src={authorImage}
+            alt=""
+            fill
+          />
+        </div>
         <div>
-          <h2 className="text-sm font-medium leading-4 text-white">{name}</h2>
-          <h3 className="text-sm leading-4 text-softPink">Verified Buyer</h3>
+          <h2 className="text-sm font-bold leading-4 text-white lg:text-lg">
+            {name}
+          </h2>
+          <h3 className="text-sm leading-4 text-softPink lg:text-lg">
+            Verified Buyer
+          </h3>
         </div>
       </div>
-      <p className="text-white text-[17px]">{review}</p>
+      <p className="text-[17px] text-white font-bold lg:text-base">{review}</p>
     </div>
   );
 };
